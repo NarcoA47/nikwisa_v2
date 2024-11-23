6d294711f984310699c4d61b8876e46605c9e9e2
Bulemu Shilukukwa <narcoa47@gmail.com>
2024-11-23 11:24:25 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/effect-fade";
-import Header from "../components/home/Header";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, EffectFade } from "swiper/modules";
-import { useState } from "react";
-import SearchBar from "@/components/SearchBar";
+// import { useState } from "react";
 import HeroFilter from "@/components/HeroFilter";
 import Categories from "@/components/Categories";
 import MiniCategories from "@/components/MiniCategories";
@@ -15,13 +11,13 @@ import PopularSearches from "@/components/PopularSearches";
 import ExploreCities from "@/components/ExploreCities";
 
 const Home: React.FC = () => {
-  const [searchLocation, setSearchLocation] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
+  // const [searchLocation, ] = useState("");
+  // const [searchQuery, ] = useState("");
 
-  const handleSearch = () => {
-    // Implement search functionality here
-    console.log(`Searching for ${searchQuery} in ${searchLocation}`);
-  };
+  // const handleSearch = () => {
+  //   // Implement search functionality here
+  //   console.log(`Searching for ${searchQuery} in ${searchLocation}`);
+  // };
 
   return (
     <div>
