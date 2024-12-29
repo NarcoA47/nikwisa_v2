e64e388cc5ef3966bb6fa475703355b9d17e088b
Narcob47 <137599138+Narcob47@users.noreply.github.com>
2024-12-29 09:24:04 +0200

    changes

--- src/components/StoreCard.tsx
+++ src/components/StoreCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 interface StoreCardProps {
 
